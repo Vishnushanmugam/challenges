45cf8794e26be62ccc072116af0e6c15010ca608
Vishnushanmugam <vishnu944244@gmail.com>
2023-12-06 10:10:36 +0530

    new

--- python/flames.py
+++ python/flames.py
@@ -1,42 +1,61 @@
 import tkinter as tk
-
 def Flames():
-    name1=entry_name1.get()
-    name2=entry_name2.get()
-    names=name1+name2
-
-    for n in names:
-        if names.count(n) !=1:
-            names=names.replace(n,"")
+    a=entry_name1.get()
+    b=entry_name2.get()
+    for i in a:
+        if a.count(i) != 1:
+            a = a.replace(" ", "")
             
-    number=len(names)%6
-    
-    f={
-        0:"SIBLINGS",
-        1:"FRIENDS",
-        2:"LOVE",
-        3:"AFFECTION",
-        4:"MARRIAGE",
-        5:"ENEMY",
-        
-    }
-    
-    rel=f.get(number)
+    for n in b:
+        if b.count(n) != 1:
+            b = b.replace(" ", "")
 
-    result_label.config(text="your relationship is: " +rel)
+    s = a + b
+    for f in s:
+        if s.count(f) != 1:
+            s = s.replace(" ", "")
+
+    w = list(s)
+
+    process = True
+
+    while process:
+        rl = w
+        cl = rl[0]
+        process = "*" in cl
+        if process:
+            si = cl.index("*")
+            a = cl[:si]
+            b = cl[si + 1:]
+        
+    count = len(a) + len(b)
+
+    result = ["FRIEND", "LOVE", "AFFECTION", "MARRIAGE", "ENEMY", "SIBILINGS"]
+
+    while len(result) > 1:
+        sid = (count % len(result) - 1)
+        if sid >= 0:
+            right = result[sid + 1:]
+            left = result[: sid]
+            result = right + left
+        else:
+            result = result[: len(result) - 1]
+        rel=result[0]
+
+        result_label.config(text="YOUR RELATIONSHIP : " +rel)
 
 
 window=tk.Tk()
-window.title("FLAMES GAME")
+window.title("Flames game")
 window.geometry("500x500")
 
-label_header=tk.Label(window,text="JUST FOR FUN GAME",font=("Arial",15),bg="red")
+label_header=tk.Label(window,text="FLAMES",font=("Arial",15),bg="red")
 label_header.pack()
 label_name1=tk.Label(window,text="Enter your name")
 label_name1.pack()
 entry_name1=tk.Entry(window,textvariable=tk.StringVar())
 entry_name1.pack()
-label_name2=tk.Label(window,text="Enter your crush name")
+label_name2=tk.Label(window,text="Enter your partener name")
 label_name2.pack()
 entry_name2=tk.Entry(window,textvariable=tk.StringVar())
 entry_name2.pack()
