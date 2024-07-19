3f0f518958923e0f2a6c7f5b9525f0bdabb0f156
vishnu <111142872+Vishnushanmugam@users.noreply.github.com>
2024-07-19 17:00:23 +0530

    First

--- python/c7.py
+++ python/c7.py
@@ -1,16 +1,17 @@
 def add_dots(a):
-    s=a
     s=".".join(a)
     return s
 print(add_dots("test"))
+
 def remove_dots(a):
-    s=a
     s=a.replace(".","")
     return s
 print(remove_dots("t.e.s.t"))
+
 def add_dot(s):
     return ".".join(s)
-print("test")
+print(add_dot("test"))
+
 def remove_dots(s):
     return s.replace(".","")
-print("t.e.s.t")
+print(remove_dots("t.e.s.t"))
