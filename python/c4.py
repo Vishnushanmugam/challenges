a888113762c0da27164f39f73945f1d15a79ab36
vishnu <111142872+Vishnushanmugam@users.noreply.github.com>
2024-07-19 09:00:59 +0530

    First

--- python/c4.py
+++ python/c4.py
@@ -1,7 +1,10 @@
 import random
-import random
-
+#Find the coming number is odd or even number
 def random_number():
     a = int(random.uniform(1, 100))
+    if a%2:
+        print(f"The coming number is odd: {a}")
+    else:
+        print(f"The coming number is even: {a}")
     return a
 print(random_number())
